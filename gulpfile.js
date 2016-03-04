7bbe7c7ac139ac323d6fdba7aa96fd4c1d306efa
Nikita Safonov <ns3777k@gmail.com>
2016-03-04 23:25:45 +0300

    build. no tests for now

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,23 @@ const gulp = require('gulp');
 const babel = require('gulp-babel');
 const uglify = require('gulp-uglify');
 const rename = require('gulp-rename');
+const sourcemaps = require('gulp-sourcemaps');
+const clean = require('gulp-clean');
 
-gulp.task('default', () => {
+gulp.task('clean', () => {
+    return gulp.src('dist', {read: false})
+        .pipe(clean());
+});
+
+gulp.task('build', ['clean'], () => {
     return gulp.src('src/*')
-        .pipe(babel({
-            babelrc: true
-        }))
+        .pipe(babel({babelrc: true}))
         .pipe(gulp.dest('dist'))
-        .pipe(uglify())
-        .pipe(rename({
-            suffix: '.min'
-        }))
+        .pipe(sourcemaps.init())
+            .pipe(uglify())
+            .pipe(rename({suffix: '.min'}))
+        .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest('dist'));
 });
+
+gulp.task('default', ['build'], () => {});
--- test/retail.rocket.segmentator.test.js
+++
@@ -1,3 +0,0 @@
-/**
- * Created by nikita on 04.03.16.
- */
